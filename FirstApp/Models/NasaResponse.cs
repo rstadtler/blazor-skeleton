91e9d14f25fc61f5a35fff91df2c8d24a2cc4366
rstadtler <stadtler_robert@columbusstate.edu>
2023-02-22 00:38:04 +0000

    Building NASA API

---
+++ FirstApp/Models/NasaResponse.cs
@@ -0,0 +1,30 @@
+using System.Text.Json.Serialization;
+
+namespace FirstApp.Models;
+
+public record NasaResponse{
+    [JsonPropertyName("copyright")]
+    public string copyright{get;init;}
+    
+    [JsonPropertyName("date")]
+    public string date{get;init;}
+   
+    [JsonPropertyName("explanation")]
+    public string explanation{get;init;}
+    
+    [JsonPropertyName("hdurl")]
+    public string hdurl{get;init;}
+    
+    [JsonPropertyName("media_type")]
+    public string mediaType{get;init;}
+    
+    [JsonPropertyName("url")]
+    public string url{get;init;}
+    
+    [JsonPropertyName("service_version")]
+    public string serviceVersion{get;init;}
+    
+    [JsonPropertyName("title")]
+    public string title{get;init;}
+}
+//copyright, date, explanation, hdurl, media_type, url, service_version, title
