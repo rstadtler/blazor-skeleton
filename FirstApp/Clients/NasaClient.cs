b46d4d73c000d199dd0e95751f531e5c4f07f2ea
rstadtler <stadtler_robert@columbusstate.edu>
2023-02-22 01:23:47 +0000

    bug

--- FirstApp/Clients/NasaClient.cs
+++ FirstApp/Clients/NasaClient.cs
@@ -10,6 +10,6 @@ namespace FirstApp.Clients;
             _client = client;
         } 
         public async Task<NasaResponse> getNasa(){
-            return await _client.getFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?api_key=IZ2T38wTGwOcpdfVyVOwu5allViUb5bvQ5DZEhOb");
+            return await _client.GetFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?api_key=IZ2T38wTGwOcpdfVyVOwu5allViUb5bvQ5DZEhOb");
         }
     }
