5e4caeab256185926df86da7cb174401d9cbf028
rstadtler <stadtler_robert@columbusstate.edu>
2023-02-22 16:55:45 +0000

    Testing New Button

--- FirstApp/Clients/NasaClient.cs
+++ FirstApp/Clients/NasaClient.cs
@@ -12,4 +12,7 @@ namespace FirstApp.Clients;
         public async Task<NasaResponse> GetNasa(){
             return await _client.GetFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?api_key=IZ2T38wTGwOcpdfVyVOwu5allViUb5bvQ5DZEhOb");
         }
+        public async Task<NasaResponse> GetDate(DateTime newDate ){
+            return await _client.GetFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?date=@newDate");
+        }
     }
