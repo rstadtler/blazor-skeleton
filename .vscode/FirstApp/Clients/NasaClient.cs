01f1aebb8d10139f1fc64bf654d18f75d73115e5
rstadtler <stadtler_robert@columbusstate.edu>
2023-02-23 07:41:26 +0000

    Copy

---
+++ .vscode/FirstApp/Clients/NasaClient.cs
@@ -0,0 +1,18 @@
+using System.Net.Http.Json;
+using FirstApp.Models;
+
+namespace FirstApp.Clients;
+    public class NasaClient{
+
+        private readonly HttpClient _client;
+
+        public NasaClient(HttpClient client){
+            _client = client;
+        } 
+        public async Task<NasaResponse> GetNasa(){
+            return await _client.GetFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?api_key=IZ2T38wTGwOcpdfVyVOwu5allViUb5bvQ5DZEhOb");
+        }
+        public async Task<NasaResponse> NasaRecord(string newDate){
+            return await _client.GetFromJsonAsync<NasaResponse>("https://api.nasa.gov/planetary/apod?api_key=IZ2T38wTGwOcpdfVyVOwu5allViUb5bvQ5DZEhOb&date="+newDate);
+        }
+    }
---
+++ .vscode/FirstApp/TodoItem.cs
@@ -0,0 +1,4 @@
+public class TodoItem{
+    public string? Item {get;set;}
+    public bool Done {get;set;}
+}
