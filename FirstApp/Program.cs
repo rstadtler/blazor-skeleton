79386773ccacc0e09957bf4f267bfd43ec024ed4
rstadtler <stadtler_robert@columbusstate.edu>
2023-03-01 00:07:28 +0000

    Added Attributes

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,8 +2,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FirstApp;
 using FirstApp.Clients;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>(provider=> provider.GetRequiredService<CustomAuthenticationStateProvider>());
+builder.Services.AddAuthorizationsCore();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
