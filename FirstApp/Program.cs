eceadedd70e65549f7d7a0b5dece16c629bb436c
rstadtler <stadtler_robert@columbusstate.edu>
2023-03-01 01:20:08 +0000

    More Testing

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped<CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<CustomAuthenticationStateProvider>(provider=> provider.GetRequiredService<CustomAuthenticationStateProvider>());
-builder.Services.AddAuthorizationsCore();
+builder.Services.AddAuthorizationCore();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
