26297aebf63f2a1f02e91e6e2c88e2a8fcb83fc7
rstadtler <stadtler_robert@columbusstate.edu>
2023-03-01 01:37:23 +0000

    Is it fixed

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped<CustomAuthenticationStateProvider>();
-builder.Services.AddScoped<CustomAuthenticationStateProvider>(provider=> provider.GetRequiredService<CustomAuthenticationStateProvider>());
+builder.Services.AddScoped<AuthenticationStateProvider>(provider=> provider.GetRequiredService<CustomAuthenticationStateProvider>());
 builder.Services.AddAuthorizationCore();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
